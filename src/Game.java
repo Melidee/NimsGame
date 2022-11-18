2d8189850401b1ccff5211487650cd9dd0c7c1e3
Melidee <w.ardecan@gmail.com>
2022-11-18 18:43:56 +0000

    Green

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Game {
     private int pieces;
     private Player p1;
@@ -6,56 +7,56 @@ public class Game {
     private Player currentPlayer;
     private Scanner sc = new Scanner(System.in);
 
-    //Sets up the game with a random amount of pieces between 10 and 50
-    //Sets up the players so they can be accessed
-    public Game(Player p1, Player p2){
-        pieces = 0;//To Do: Grab a random value between 10 and 50
+    // Sets up the game with a random amount of pieces between 10 and 50
+    // Sets up the players so they can be accessed
+    public Game(Player p1, Player p2) {
+        pieces = (int) (Math.random() * 40) + 10;
         this.p1 = p1;
         this.p2 = p2;
     }
 
-    //Allows a player to take a specific amount of pieces.
-    //Adds the amount of pieces taken to the user's score.
-    public int takePiece(){
-        System.out.println("There are "+ pieces+" remaining!");
-        int take;
-       //TO DO: Grab the user amount of pieces and repeat if it not allowed.
-        while (!isLegal(take)){
-            
+    // Allows a player to take a specific amount of pieces.
+    // Adds the amount of pieces taken to the user's score.
+    public int takePiece() {
+        System.out.println("There are " + pieces + " remaining!");
+        int take = -1;
+        while (!isLegal(take)) {
+            System.out.println("How many pieces would you like to take?");
+            take = sc.nextInt();
         }
-        //TO DO: Adjust the pieces
-
-        System.out.println("There are "+ pieces+" remaining!");
+        pieces -= take;
+        System.out.println("There are " + pieces + " remaining!");
         System.out.println("-----------------------");
         return take;
     }
 
-    
-    //**TO DO**//
     public Player getNextPlayer(){
     //Changes which players turn it is and returns the current player.
-       
-        return currentPlayer;
+        if (currentPlayer.equals(p1)) {
+            currentPlayer = p2;
+            return p2;
+        } else {
+            currentPlayer = p1;
+            return p1;
+        }
     }
 
-    //Checks whether or not the user's requested move is allowed or not.
-    public boolean isLegal(int x){
-
-        //TO DO
-
-        return false;// Change appropriately
+    // Checks whether or not the user's requested move is allowed or not.
+    public boolean isLegal(int x) {
+        return (x > 0) && x <= (int) pieces / 2;
     }
 
+    // DO NOT CHANGE
+    public boolean isComplete() {
 
-    //DO NOT CHANGE
-    public boolean isComplete(){
-
-        if (pieces == 0){ return true;}
+        if (pieces == 0) {
+            return true;
+        }
         return false;
     }
 
-    //DO NOT CHANGE
-    public void setFirstPlayer(Player p){
+    // DO NOT CHANGE
+    public void setFirstPlayer(Player p) {
         currentPlayer = p;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
 
         //Asks the user to input thier name for the player
         System.out.println("What is player1's name?: ");
-        //TO DO: Create Player 1
+        Player p1 = new Player(sc.nextLine());
 
         //Asks the user to input their name for the player.
         System.out.println("What is player2's name?: ");
-        //TO DO: Create Player 2
+        Player p2 = new Player(sc.nextLine());
 
         String again = "Literally any string";
         Player currentPlayer;
@@ -17,17 +17,21 @@ public class Main {
         //Runs the game
         while (!again.equals("q")){
             //Generates the game
-            //TO DO: Create "game"
-
-            //TO DO: Set the a random player as the current player
+            Game game = new Game(p1, p2);
+            if (Math.random() > 0.5) {
+                currentPlayer = p1;
+            } else {
+                currentPlayer = p2;
+            }
             
 
             game.setFirstPlayer(currentPlayer);
             //This is the loop in which the game will be played
             while(!game.isComplete()){
-               //TO DO
+                System.out.println(currentPlayer.getName() + " it is your turn");
+                currentPlayer.adjustScore(game.takePiece());
 
-               currentPlayer = game.getNextPlayer();
+                currentPlayer = game.getNextPlayer();
             }
             System.out.println(currentPlayer.getName()+ " Won!!");
             System.out.println(p1.getName() +" had removed "+ p1.getScore()+" pieces!");
@@ -37,5 +41,6 @@ public class Main {
             again = sc.nextLine();
         }
         System.out.println("Thank you for playing!");
+        sc.close();
     }
 }
